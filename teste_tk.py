52f5b830213676910b9abb4d11d2d674e7a5bddf
LucasAltave <lucas.pereira@altave.com.br>
2026-03-13 01:09:52 -0300

    Update teste_tk.py
    Apenas um ajuste, retornando ao correto

--- teste_tk.py
+++ teste_tk.py
@@ -7,19 +7,4 @@ tk.Label(root, text="Tkinter OK").pack(pady=20)
 root.mainloop()
 import tkinter as tk
 
-def criar_interface():
-    root = tk.Tk()
-    root.title("Minha Aplicação com Tkinter")
 
-    # Criação de widgets
-    label = tk.Label(root, text="Bem-vindo ao meu app!")
-    label.pack(pady=20)
-
-    # Botão para fechar
-    button = tk.Button(root, text="Fechar", command=root.quit)
-    button.pack(pady=10)
-
-    root.mainloop()
-
-# Chamar a função para criar a interface
-criar_interface()
